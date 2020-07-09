fc0f61ecb696ccb5d0c891d4467b8d4741853665
yokn <discolouredattic@gmail.com>
2020-07-09 14:28:06 +0300

    edit comments

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,29 +1,35 @@
 # frozen_string_literal: true
 
+# Initial plan:
 # Take an array from the user and store it
+# Create a new array to store the results
+# Until end of the array is reached:
+#     Compare the next two chars (with the spaceship operator)
+#     If the second char is bigger than the first char:
+#       Copy their values to the results array swapped
+#     Else
+#       Copy their values to the results array
+#     If the last char is reached:
+#       Reduce the array length by one
+#   while index < user_array_length
+# Print the results array
+
 def bubble_sort(user_array)
-  # Create a new array to store the results
   change = true
-  # Until end of the array is reached:
-  #   Compare the next two chars (with the spaceship operator)
-  #   If the second char is bigger than the first char:
-  #     Copy their values to the results array swapped
-  #   Else
-  #     Copy their values to the results array
-  #   If the last char is reached:
-  #     Reduce the array length by one
-  # while index < user_array_length
+
   until change == false
     change = false
     user_array.each_with_index do |_value, index|
       next if index >= user_array.length - 1
       next unless user_array[index] > user_array[index + 1]
 
+      # neat trick to swap two numbers without a temporary variable
+      # https://coderwall.com/p/be_1va/swap-two-elements-of-an-array-in-ruby
       user_array[index], user_array[index + 1] = user_array[index + 1], user_array[index]
       change = true
     end
   end
   user_array
 end
-# Print the results array
+
 p bubble_sort([4, 3, 78, 2, 0, 2])
