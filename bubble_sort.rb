4b0220de0cca80df31c421ad60bfec404334a59e
yokn <discolouredattic@gmail.com>
2020-07-09 14:17:45 +0300

    refactoring

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,7 +4,6 @@
 def bubble_sort(user_array)
   # Create a new array to store the results
   change = true
-  i = 1
   # Until end of the array is reached:
   #   Compare the next two chars (with the spaceship operator)
   #   If the second char is bigger than the first char:
@@ -15,26 +14,16 @@ def bubble_sort(user_array)
   #     Reduce the array length by one
   # while index < user_array_length
   until change == false
-    puts "BEGIN loop #{i}"
     change = false
     user_array.each_with_index do |_value, index|
-      puts "index is: #{index}"
-      # puts user_array[index]
-      # puts user_array[index + 1]
-      # next if change == true
       next if index >= user_array.length - 1
       next unless user_array[index] > user_array[index + 1]
 
-      puts "#{user_array[index]} is bigger than #{user_array[index + 1]}"
       user_array[index], user_array[index + 1] = user_array[index + 1], user_array[index]
       change = true
-      puts "change is: #{change}"
     end
-    puts change
-    puts "END loop #{i}"
-    i += 1
   end
-  puts "sorted array: #{user_array}"
+  user_array
 end
 # Print the results array
-bubble_sort([4, 3, 78, 2, 0, 2])
+p bubble_sort([4, 3, 78, 2, 0, 2])
