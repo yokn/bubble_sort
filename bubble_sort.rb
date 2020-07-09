4957e393e3dec497223dc84493cee1cf4dac2c69
yokn <discolouredattic@gmail.com>
2020-07-09 13:39:49 +0300

    debugging

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,25 +13,27 @@ def bubble_sort(user_array)
   #     Copy their values to the results array
   #   If the last char is reached:
   #     Reduce the array length by one
+  # while index < user_array_length
   user_array.each_with_index do |_value, index|
-    while index < user_array_length
-      puts index
-      puts user_array_length
-      # puts user_array[index]
-      # puts user_array[index + 1]
-      if user_array[index + 1] < user_array[index]
+    puts "index is: #{index}"
+    puts "user_array_length is: #{user_array_length}"
+    # puts user_array[index]
+    # puts user_array[index + 1]
+    unless index >= user_array_length - 1
+      if user_array[index + 1] > user_array[index]
+        puts "abc, #{user_array[index + 1]} is bigger than #{user_array[index]}"
         results_array[index] = user_array[index + 1]
         results_array[index + 1] = user_array[index]
-        puts 'hi'
       else
+        puts "hello, #{user_array[index + 1]} is smaller than or equal to #{user_array[index]}"
         results_array[index] = user_array[index]
         results_array[index + 1] = user_array[index + 1]
-        puts 'hello'
       end
     end
-    user_array_length -= 1
   end
-  puts results_array
+  #   user_array_length -= 1
+  # end
+  puts "sorted array: #{results_array}"
 end
 # Print the results array
-puts bubble_sort([4, 3, 78, 2, 0, 2])
+bubble_sort([4, 3, 78, 2, 0, 2])
