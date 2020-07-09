2c73b12a2977f72dc5128f99142fe102ed9178c3
yokn <discolouredattic@gmail.com>
2020-07-09 14:14:50 +0300

    working

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,8 +3,8 @@
 # Take an array from the user and store it
 def bubble_sort(user_array)
   # Create a new array to store the results
-  results_array = []
-  user_array_length = user_array.length
+  change = true
+  i = 1
   # Until end of the array is reached:
   #   Compare the next two chars (with the spaceship operator)
   #   If the second char is bigger than the first char:
@@ -14,26 +14,27 @@ def bubble_sort(user_array)
   #   If the last char is reached:
   #     Reduce the array length by one
   # while index < user_array_length
-  user_array.each_with_index do |_value, index|
-    puts "index is: #{index}"
-    puts "user_array_length is: #{user_array_length}"
-    # puts user_array[index]
-    # puts user_array[index + 1]
-    unless index >= user_array_length - 1
-      if user_array[index + 1] > user_array[index]
-        puts "abc, #{user_array[index + 1]} is bigger than #{user_array[index]}"
-        results_array[index] = user_array[index + 1]
-        results_array[index + 1] = user_array[index]
-      else
-        puts "hello, #{user_array[index + 1]} is smaller than or equal to #{user_array[index]}"
-        results_array[index] = user_array[index]
-        results_array[index + 1] = user_array[index + 1]
-      end
+  until change == false
+    puts "BEGIN loop #{i}"
+    change = false
+    user_array.each_with_index do |_value, index|
+      puts "index is: #{index}"
+      # puts user_array[index]
+      # puts user_array[index + 1]
+      # next if change == true
+      next if index >= user_array.length - 1
+      next unless user_array[index] > user_array[index + 1]
+
+      puts "#{user_array[index]} is bigger than #{user_array[index + 1]}"
+      user_array[index], user_array[index + 1] = user_array[index + 1], user_array[index]
+      change = true
+      puts "change is: #{change}"
     end
+    puts change
+    puts "END loop #{i}"
+    i += 1
   end
-  #   user_array_length -= 1
-  # end
-  puts "sorted array: #{results_array}"
+  puts "sorted array: #{user_array}"
 end
 # Print the results array
 bubble_sort([4, 3, 78, 2, 0, 2])
