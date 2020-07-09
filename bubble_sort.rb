517b6f06a099db8a473218d81fd5791e4ae60f2f
yokn <discolouredattic@gmail.com>
2020-07-09 12:33:45 +0300

    initial commit

---
+++ bubble_sort.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Take an array from the user and store it
+# Create a new array to store the results
+# Until end of the array is reached:
+#   Compare the next two chars (with the spaceship operator)
+#   If the second char is bigger than the first char:
+#     Copy their values to the results array swapped
+#   Else copy their values to the results array
+#   If the last char is reached:
+#     Reduce the array length by one
+# Print the results array
